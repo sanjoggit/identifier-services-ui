b128c6848644f89ad54dd01ad6505a0e970383a4
Shrestha Sanjog R <srshrest@lx9-9815-007.lib.helsinki.fi>
2019-04-25 12:02:28 +0300

    react-router setupinitiated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Navbar from './components/navbar/Navbar';
 
 class App extends Component{
     render(){
         return(
-            <Navbar />
+            <Router>
+                <Navbar />
+            </Router>
         )
     }
 }
