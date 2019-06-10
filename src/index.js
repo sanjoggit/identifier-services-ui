03d0320d361cdd847164a820a8cb2c93b96aaeef
Codefeak <codefever17@gmail.com>
2019-06-10 13:17:32 +0300

    Revert "Dev (#7)"
    This reverts commit 7e2b01582f4e909b6fbb9510c8e25fd556cf5aa2.

--- src/index.js
+++ src/index.js
@@ -1,45 +1,27 @@
-/* eslint-disable no-undef */
 /**
- *
- * @licstart  The following is the entire license notice for the JavaScript code in this file.
- *
- * UI microservice of Identifier Services
- *
- * Copyright (C) 2019 University Of Helsinki (The National Library Of Finland)
- *
- * This file is part of identifier-services-ui
- *
- * identifier-services-ui program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * identifier-services-ui is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * @licend  The above is the entire license notice
- * for the JavaScript code in this file.
- *
- */
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import {Provider} from 'react-redux';
-import allReducers from './store/reducers';
-import thunk from 'redux-thunk';
-import {createStore, applyMiddleware, compose} from 'redux';
-
-const store = createStore(
-	allReducers,
-	compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
-
-ReactDOM.render(
-	<Provider store={store}>
-		<App/>
-	</Provider>, document.getElementById('app'));
+*
+* @licstart  The following is the entire license notice for the JavaScript code in this file.
+*
+* UI microservice of Identifier Services 
+*
+* Copyright (C) 2019 University Of Helsinki (The National Library Of Finland)
+*
+* This file is part of identifier-services-ui
+*
+* identifier-services-ui program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* identifier-services-ui is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+* @licend  The above is the entire license notice
+* for the JavaScript code in this file.
+*
+*/
